1ab7437681ecef51a47f71236f2117730ee971a4
Joshua Lee <jjlee2920@gmail.com>
2023-11-12 09:30:25 -0500

    Update to include submissions channel, leaderboard channel, automatic image deletion, send private message on improper submission

--- discordIRC.go
+++ discordIRC.go
@@ -9,6 +9,8 @@ import (
 	"net/url"
 	"os"
 	"os/signal"
+	"sort"
+	"strconv"
 	"strings"
 	"syscall"
 )
@@ -23,6 +25,7 @@ func startDiscordIRC() {
 
 	// Create handler for listening for submission messages
 	session.AddHandler(listenForSubmission)
+	session.AddHandler(AddNewMember)
 
 	// Send intent
 	session.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
@@ -50,21 +53,82 @@ func listenForSubmission(session *discordgo.Session, message *discordgo.MessageC
 		return
 	}
 
-	for _, submissionPicture := range message.Attachments {
-		_, err := session.ChannelMessageSend(message.ChannelID, submissionPicture.ProxyURL)
-		if err != nil {
+	// Split the names into an array by , then make an empty array with those names as keys for an easier lookup
+	// instead of running a for loop inside a for loop when adding clan points
+	whitespaceStrippedMessage := strings.Replace(message.Content, ", ", ",", -1)
+	whitespaceStrippedMessage = strings.Replace(whitespaceStrippedMessage, " ,", ",", -1)
+	names := strings.Split(whitespaceStrippedMessage, ",")
+
+	// Before adding clanpoints, ensure that all the names used in the submission is valid and already created
+	// in the #ponies-signup channel
+	for _, name := range names {
+		// Ensure that this person does not exist in the submissions map currently
+		if _, ok := submissions[name]; !ok {
+			// Create a private channel with the user submitting (will reuse if one exists)
+			channel, err := session.UserChannelCreate(message.Author.ID)
+			if err != nil {
+				return
+			}
+
+			// Send a message on that channel
+			_, err = session.ChannelMessageSend(channel.ID, "Non clan member used in this submission. "+
+				"Please add the user: \""+name+"\" using the https://discord.com/channels/1172535371905646612/1173253913303056524 channel and resubmit the screenshot with the names.")
+			if err != nil {
+				return
+			}
+
+			// Once everything is finished, delete the message from the submission channel
+			err = session.ChannelMessageDelete(config.DiscSubChan, message.ID)
+			if err != nil {
+				return
+			}
 			return
 		}
-
-		downloadSubmissionScreenshot(submissionPicture.ProxyURL)
 	}
 
-	// If hello is read, world is responded
-	if message.Content == "hello" {
-		_, err := session.ChannelMessageSend(message.ChannelID, "hello")
-		if err != nil {
-			return
-		}
+	numberOfSubmissions := 0
+
+	// Iterate through all the pictures and download them
+	for _, submissionPicture := range message.Attachments {
+		downloadSubmissionScreenshot(submissionPicture.ProxyURL)
+		numberOfSubmissions++
+	}
+
+	// Iterate over the all the names in the submissions and add the number of submissions to their clan points
+	for _, name := range names {
+		submissions[name] = submissions[name] + numberOfSubmissions
+	}
+
+	// Update the #cp-leaderboard
+	updateLeaderboard(session)
+
+	// Once everything is finished, delete the message from the submission channel
+	err := session.ChannelMessageDelete(config.DiscSubChan, message.ID)
+	if err != nil {
+		return
+	}
+}
+
+// updateLeaderboard will update the cp-leaderboard channel in discord with a new ranking of everyone in the clan
+func updateLeaderboard(session *discordgo.Session) {
+	// Update the #cp-leaderboard
+	keys := make([]string, 0, len(submissions))
+	for key := range submissions {
+		keys = append(keys, key)
+	}
+
+	// Sort the map based on the values
+	sort.SliceStable(keys, func(i, j int) bool {
+		return submissions[keys[i]] > submissions[keys[j]]
+	})
+
+	leaderboard := ""
+	for _, k := range keys {
+		leaderboard = leaderboard + k + ":" + strconv.Itoa(submissions[k]) + "\n"
+	}
+	_, err := session.ChannelMessageSend(config.DiscLeaderboardChan, leaderboard)
+	if err != nil {
+		return
 	}
 }
 
@@ -99,4 +163,29 @@ func downloadSubmissionScreenshot(submissionLink string) {
 	_, err = io.Copy(file, resp.Body)
 
 	defer file.Close()
+}
+
+func AddNewMember(session *discordgo.Session, message *discordgo.MessageCreate) {
+	// Don't handle message if it's created by the discord bot
+	// Also, don't handle messages other than ones send in the submission channel
+	if message.Author.ID == session.State.User.ID || message.ChannelID != config.DiscSignUpChan {
+		return
+	}
+
+	//TODO: SCRUB THE USERNAME SUBMITTED
+	newMember := message.Content
+
+	// Ensure that this person does not exist in the submissions map currently
+	if _, ok := submissions[newMember]; !ok {
+		submissions[newMember] = 0
+		_, err := session.ChannelMessageSend(message.ChannelID, "Successfully added new member: "+newMember)
+		if err != nil {
+			return
+		}
+	} else {
+		_, err := session.ChannelMessageSend(message.ChannelID, "Member: "+newMember+" already exists.")
+		if err != nil {
+			return
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"github.com/gemalto/flume"
 	"github.com/spf13/viper"
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
 )
 
 type Config struct {
-	DiscBotToken string `mapstructure:"DISCORD_BOT_TOKEN"`
-	DiscServURL  string `mapstructure:"DISCORD_SERVER_URL"`
-	DiscSubChan  string `mapstructure:"DISCORD_SUBMISSION_CHANNEL"`
+	DiscBotToken        string `mapstructure:"DISCORD_BOT_TOKEN"`
+	DiscServURL         string `mapstructure:"DISCORD_SERVER_URL"`
+	DiscSubChan         string `mapstructure:"DISCORD_SUBMISSION_CHANNEL"`
+	DiscSignUpChan      string `mapstructure:"DISCORD_SIGNUP_CHANNEL"`
+	DiscLeaderboardChan string `mapstructure:"DISCORD_LEADERBOARD_CHANNEL"`
 }
 
 var config Config
+var submissions map[string]int
 
 func init() {
 	// Initialize the Viper configuration ingestion and unmarshal
@@ -22,10 +32,55 @@ func init() {
 		return
 	}
 	err = viper.Unmarshal(&config)
+
+	// Read the clan points file and break it up into an array then into a map
+	f, err := os.ReadFile("submissions/clan_points.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Break up the names in the clan points file and store them on a map
+	names := strings.Split(string(f), "\n")
+	submissions = make(map[string]int)
+	for _, nameAndCp := range names {
+		if len(nameAndCp) == 0 {
+			continue
+		}
+		parts := strings.Split(nameAndCp, ":")
+		cp, _ := strconv.Atoi(parts[1])
+		submissions[parts[0]] = cp
+	}
 }
 
 func main() {
 	var log = flume.New("main")
 	log.Info("TESTING")
+
+	// Throw a go func that will capture signal interrupts and will populate the submissions file
+	go func() {
+		sigchan := make(chan os.Signal)
+		signal.Notify(sigchan,
+			// https://www.gnu.org/software/libc/manual/html_node/Termination-Signals.html
+			syscall.SIGTERM, // "the normal way to politely ask a program to terminate"
+			syscall.SIGINT,  // Ctrl+C
+			syscall.SIGQUIT, // Ctrl-\
+			syscall.SIGKILL, // "always fatal", "SIGKILL and SIGSTOP may not be caught by a program"
+			syscall.SIGHUP,  // "terminal is disconnected"
+		)
+		<-sigchan
+
+		// Once the program is interrupted, remake the clan_points.txt file with the locally stored submissions map
+		f, _ := os.Create("submissions/clan_points.txt")
+		datawriter := bufio.NewWriter(f)
+		for name, cp := range submissions {
+			cpToString := strconv.Itoa(cp)
+			_, _ = datawriter.WriteString(name + ":" + cpToString + "\n")
+		}
+		datawriter.Flush()
+		f.Close()
+
+		os.Exit(0)
+	}()
+
 	startDiscordIRC()
 }
